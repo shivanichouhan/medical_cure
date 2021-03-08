dff6ff5fac1c74a6c1240b1b2622caf39ba9e117
tekeshwar810 <tekeshwar810@gmail.com>
2021-03-08 10:07:43 +0530

    8march

--- routes/Doctor/Registration.js
+++ routes/Doctor/Registration.js
@@ -2,8 +2,4 @@ var express = require('express');
 var router = express.Router();
 const{ doctor_reg } = require("../../controller/Doctor/registration")
 router.post("/doctor_regis",doctor_reg)
-
-
-
-
 module.exports = router;
