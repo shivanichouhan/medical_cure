4378f8f7456525bd0b50d145edc32096862f8571
tekeshwar810 <tekeshwar810@gmail.com>
2021-03-10 15:30:08 +0530

    10mar1st

--- routes/helth_worker/city_list.js
+++ routes/helth_worker/city_list.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { district_add,list_state,list_district } = require("../../controller/helth_worker/city_list")
 
 router.get("/health_worker/list_state",list_state)
-router.get("/health_worker/list_district",list_district)
+router.post("/health_worker/list_district",list_district)
 router.post("/health_worker/add_state_district",district_add)
 
 module.exports = router;
