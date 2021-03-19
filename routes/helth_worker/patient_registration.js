bcc67def64699a516a17446d77d504210c7df132
tekeshwar810 <tekeshwar810@gmail.com>
2021-03-19 18:27:15 +0530

    19march18:27

--- routes/helth_worker/patient_registration.js
+++ routes/helth_worker/patient_registration.js
@@ -4,7 +4,7 @@ const{ create,patient_list,patient_verfiy,patient_info } = require("../../contro
 const upload = require("../../handler/multer")
 const { chk_helth_status,checkLogin } = require("../../auth")
 
-router.get("/patient_list/:userId",checkLogin,patient_list)
+router.get("/patient_list/:userId",patient_list)
 router.post("/patient_registration/:userId",create)
 router.post("/patient_mobile_verfiy/:patientId",patient_verfiy)
 router.put("/patient_details_reg/:patientId",upload.single('patient_img'),patient_info)
