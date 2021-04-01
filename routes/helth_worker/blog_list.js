05dacad5dbb6ba9e3d459da165a9f65b03215e8c
shivanjichouhan <shivanichouhan144@gmail.com>
2021-04-01 13:11:00 +0530

    edit pertucular blog data

--- routes/helth_worker/blog_list.js
+++ routes/helth_worker/blog_list.js
@@ -3,6 +3,6 @@ var router = express.Router();
 const {list_blog,pertucularBlog} = require("../../controller/helth_worker/blog_list")
 
 router.get("/blog_list",list_blog)
-router.get("blog_data/:blog_id",pertucularBlog)
+router.get("/blog_data/:blog_id",pertucularBlog)
 
 module.exports = router;
