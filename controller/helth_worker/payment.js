d283848200a324fc40e4f6f0af553d3fb77b0c30
tekeshwar810 <tekeshwar810@gmail.com>
2021-04-06 10:23:31 +0530

    132

--- controller/helth_worker/payment.js
+++ controller/helth_worker/payment.js
@@ -3,8 +3,8 @@ const shortid = require('shortid')
 const Crypto =require('crypto')
 
 var razorpay = new Razorpay({
-    key_id:'rzp_live_uroygFNlI2ROD9',
-    key_secret:'RO60jVm7adVvv1KCagTT92ZL'
+    key_id:'rzp_test_71bMsZX0h63757',
+    key_secret:'BaXwG1Ey4z1m440ZsQZPcV5b'
 })
 
 exports.gen_orderId = async(req,res)=>{
@@ -24,7 +24,7 @@ try{
     data.amount = response.amount
     data.order_id = response.id,
     data.currency = response.currency
-    data. key_id = 'rzp_live_uroygFNlI2ROD9'
+    data. key_id = 'rzp_test_71bMsZX0h63757'
     console.log(response) 
 
     res.send({code:200,msg:data})
@@ -34,8 +34,8 @@ try{
 }
 
 exports.payment_verfiy = (req,res)=>{
-    var body = req.body.razorpay_order_id + "|" + req.body.razorpay_payment_id;
-    var exp_signature = Crypto.createHmac('sha256','RO60jVm7adVvv1KCagTT92ZL')
+    var body = req.body.razorpay_order_id+ "|" +req.body.razorpay_payment_id;
+    var exp_signature = Crypto.createHmac('sha256','BaXwG1Ey4z1m440ZsQZPcV5b')
                     .update(body.toString())
                     .digest('hex')
                     console.log('real_sign',req.body.razorpay_signature)
