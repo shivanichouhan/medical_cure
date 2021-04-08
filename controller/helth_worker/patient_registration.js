b966bbfc3272eb40bc6f903d49a1fbf9eae53b1e
shivanjichouhan <shivanichouhan144@gmail.com>
2021-04-08 19:14:45 +0530

    edit accepted dataj

--- controller/helth_worker/patient_registration.js
+++ controller/helth_worker/patient_registration.js
@@ -195,6 +195,42 @@ exports.accepted_patient_list = (req, res) => {
         })
 }
 
+exports.acceptedPatient_data = (req, res) => {
+    const { doctor_id,patient_id } = req.body
+
+    const arr = []
+    patient.find({ $and: [{ status: "accepted", doctor_id: doctor_id }] })
+        .exec(async (err, List) => {
+            if (err) {
+                res.json({ code: 400, msg: 'patient list not found' })
+            }
+            else {
+                await Promise.all(List.map(async (items) => {
+                    const obj = {}
+                    const helth_workerdata = await helth_workers.findOne({ _id: items.health_worker_id })
+                    obj.helthwork_username = helth_workerdata.username;
+                    obj.health_worker_id = helth_workerdata._id
+                    obj.patient_id = items._id
+                    obj.patient_name = items.patient_name
+                    obj.status = items.status
+                    obj.createdAt = items.createdAt
+                    obj.patient_img = items.patient_img
+                    obj.mobile = items.mobile,
+                        obj.disease = "High Blood Sugar"
+                    obj.address = " "
+                    obj.doctor_id = doctor_id
+                    arr.push(obj)
+                })).then((response) => {
+                })
+                res.json({ code: 200, msg: arr })
+            }
+        })
+
+}
+
+
+
+
 exports.status_patient = (req, res) => {
     const { doctor_id } = req.body
     const arr = []
