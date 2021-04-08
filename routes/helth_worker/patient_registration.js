707ff50a1a5f466a5b9b3488e141da9c246a6acd
shivanjichouhan <shivanichouhan144@gmail.com>
2021-04-08 19:09:30 +0530

    edit accepted data

--- routes/helth_worker/patient_registration.js
+++ routes/helth_worker/patient_registration.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {appoinment_status, create, patient_list, patient_verfiy, patient_info, search_patient, status_patient } = require("../../controller/helth_worker/patient_registration")
+const {appoinment_status, create,accepted_patient_list, patient_list, patient_verfiy, patient_info, search_patient, status_patient } = require("../../controller/helth_worker/patient_registration")
 // const { appoinment_status, create, patient_list, patient_verfiy, patient_info, search_patient } = require("../../controller/helth_worker/patient_registration")
 const upload = require("../../handler/multer")
 const { chk_helth_status, checkLogin } = require("../../auth")
@@ -12,5 +12,6 @@ router.post("/patient_registration/:userId", create)
 router.post("/patient_mobile_verfiy/:patientId", patient_verfiy)
 router.put("/patient_details_reg/:patientId", upload.single('patient_img'), patient_info)
 router.post("/patient/patient_status", status_patient)
+router.post("/patient/accepted_patient_list",accepted_patient_list)
 
 module.exports = router;
