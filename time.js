12dd9747e6716d7832ee1de62bd7d358fb349786
tekeshwar810 <tekeshwar810@gmail.com>
2021-04-10 18:23:47 +0530

    fds

---
+++ time.js
@@ -0,0 +1,14 @@
+let options = {
+    timeZone: 'Asia/Kolkata',
+    hour: 'numeric',
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+},
+formatter = new Intl.DateTimeFormat([], options);
+var a =(formatter.format(new Date()));
+var str = a
+var h = str.split(",");
+console.log(h[0],h[1])
