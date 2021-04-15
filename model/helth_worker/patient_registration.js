5de22d95135c0484315d77b171f6f625db4f7ff8
shivanichauhan18 <shivanic18@navgurukul.org>
2021-04-15 15:28:40 +0530

    edit details

--- model/helth_worker/patient_registration.js
+++ model/helth_worker/patient_registration.js
@@ -2,87 +2,6 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema
 
 const patientSchema = new schema(
-<<<<<<< HEAD
-    {
-        email: {
-            type: String
-        },
-        gmailId: {
-            type: String
-        },
-        user_name: {
-            type: String
-        },
-        patient_id: {
-            type: String
-        },
-        reg_type: {
-            type: String
-        },
-        health_worker_name: {
-            type: String
-        },
-        patient_name: {
-            type: String
-        },
-        patient_img: {
-            type: String
-        },
-        mobile: {
-            type: String
-        },
-        otp: {
-            type: String
-        },
-        mob_verify: {
-            type: Boolean,
-            default: false
-        },
-        age: {
-            type: String
-        },
-        gender: {
-            type: String
-        },
-        height: {
-            type: String
-        },
-        weight: {
-            type: String
-        },
-        Appoinment: [{
-            type: schema.Types.ObjectId,
-            ref: "appoinment"
-        }],
-        health_worker_id: {
-            type: String
-        },
-        disease: {
-            type: String
-        },
-        location: {
-            type: String
-        },
-        p_reg: {
-            type: Boolean
-        },
-        patient_status: {
-            type: String,
-        },
-        prescription: [{
-            type: schema.Types.ObjectId,
-            ref: "prescription"
-        }],
-        bearer_token: {
-            type: String
-        },
-        status: {
-            type: String,
-            enum: ['ongoing', "booked", 'accepted', "completed", "cancelled"]
-        }, doctor_id: { type: String }
-    },
-    { timestamps: true }
-=======
   {
     email:{
         type:String
@@ -166,7 +85,6 @@ const patientSchema = new schema(
     },doctor_id:{type:String}
   },
   { timestamps: true }
->>>>>>> origin/admin_panel
 )
 
 module.exports = mongoose.model('patient', patientSchema)
