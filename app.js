2d9f2dc3408a303766c8c527be012070d5236c44
shivanichauhan18 <shivanic18@navgurukul.org>
2021-04-23 12:52:14 +0530

    edit details

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const Socket = require("websocket").server
+
+
+
 var express = require('express')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
@@ -7,6 +11,8 @@ var expressValidator = require('express-validator')
 var path = require('path')
 const cors = require('cors')
 const morgan = require('morgan')
+
+
 // const autoIncrement = require('mongoose-auto-increment');
 const app = express()
 const http = require('http').Server(app)
@@ -14,6 +20,9 @@ const io = require('socket.io')(http);
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, '/public')));
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/sender'));
+app.use(express.static(__dirname + '/receiver'));
 
 if (typeof localStorage === "undefined" || localStorage === null) {
   var LocalStorage = require('node-localstorage').LocalStorage;
@@ -138,6 +147,21 @@ app.set('view engine', 'ejs')
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json());
+
+
+app.get("/sender_call", (req, res) => {
+  res.sendFile(
+      path.join(__dirname + '/sender/sender.html')
+  )
+}
+)
+
+app.get("/recieve_call", (req, res) => {
+  res.sendFile(
+      path.join(__dirname + '/receiver/receiver.html')
+  )
+}
+)
 
 
 app.get("/demo", (req, res) => {
@@ -318,7 +342,114 @@ io.on('connection', function (socket) {
 app.use('/api', doctor_reg)
 
 const port = process.env.PORT || 8000
+// const server = http.createServer(app)
+
 http.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
 
+
+
+const webSocket = new Socket({ httpServer: http })
+
+let users = []
+
+webSocket.on('request', (req) => {
+    const connection = req.accept()
+
+    connection.on('message', (message) => {
+        const data = JSON.parse(message.utf8Data)
+
+        const user = findUser(data.username)
+
+        switch (data.type) {
+            case "store_user":
+
+                if (user != null) {
+                    return
+                }
+
+                const newUser = {
+                    conn: connection,
+                    username: data.username
+                }
+
+                users.push(newUser)
+                console.log(newUser.username)
+                break
+            case "store_offer":
+                if (user == null)
+                    return
+                user.offer = data.offer
+                break
+
+            case "store_candidate":
+                if (user == null) {
+                    return
+                }
+                if (user.candidates == null)
+                    user.candidates = []
+
+                user.candidates.push(data.candidate)
+                break
+            case "send_answer":
+                if (user == null) {
+                    return
+                }
+
+                sendData({
+                    type: "answer",
+                    answer: data.answer
+                }, user.conn)
+                break
+            case "send_candidate":
+                if (user == null) {
+                    return
+                }
+
+                sendData({
+                    type: "candidate",
+                    candidate: data.candidate
+                }, user.conn)
+                break
+            case "join_call":
+                if (user == null) {
+                    return
+                }
+
+                sendData({
+                    type: "offer",
+                    offer: user.offer
+                }, connection)
+
+                user.candidates.forEach(candidate => {
+                    sendData({
+                        type: "candidate",
+                        candidate: candidate
+                    }, connection)
+                })
+
+                break
+        }
+    })
+
+    connection.on('close', (reason, description) => {
+        users.forEach(user => {
+            if (user.conn == connection) {
+                users.splice(users.indexOf(user), 1)
+                return
+            }
+        })
+    })
+})
+
+function sendData(data, conn) {
+    conn.send(JSON.stringify(data))
+}
+
+function findUser(username) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username == username)
+            return users[i]
+    }
+}
