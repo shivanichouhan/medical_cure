7bead43466222363ed63761d3e4c269248b46b0f
shivanichauhan18 <shivanic18@navgurukul.org>
2021-05-07 13:09:46 +0530

    edit url from service pages

--- sender/sender.js
+++ sender/sender.js
@@ -3,7 +3,7 @@
 
 // var webSocket = new WebSocket("wss://backend.xpresscure.com/socketserver", "protocolOne");
 
-var webSocket = new WebSocket("wss://184.168.122.191:35814", ["protocolOne", "protocolTwo"]);
+var webSocket = new WebSocket("wss://backend.xpresscure.com:35814", ["protocolOne", "protocolTwo"]);
 
 
 
